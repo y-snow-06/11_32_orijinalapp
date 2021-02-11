f72538722fbc42efa573dd46b1c7787e9237924e
y-snow-06 <rasukaru2hour@gmail.com>
2021-02-11 12:14:46 +0900

    SQL追加

--- php06_sample/ajax_get.php
+++ php06_sample/ajax_get.php
@@ -1,1 +1,24 @@
 <?php
+include("functions.php");
+
+$search_word = $_GET['searchWord'];
+
+$pdo = connect_to_db();
+
+$sql = "SELECT * FROM todo_table WHERE todo LIKE '%{$search_word}%'";
+
+$stmt = $pdo->prepare($sql);
+$status = $stmt->execute();
+
+if ($status == false) {
+    // SQL実行に失敗した場合はここでエラーを出力し，以降の処理を中止する
+    $error = $stmt->errorInfo();
+    echo json_encode(["error_msg" => "{$error[2]}"]);
+    exit();
+  } else {
+    // 正常にSQLが実行された場合は入力ページファイルに移動し，入力ページの処理を実行する
+    // fetchAll()関数でSQLで取得したレコードを配列で取得できる
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);  // データの出力用変数（初期値は空文字）を設定
+    echo json_encode($result);
+    exit();
+  }
